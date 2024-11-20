720cb4fecafd24f602a30d3687cd4b08c4d6074e
Vijaya Natarajan <179537302+vijayanatarajan-coder@users.noreply.github.com>
2024-11-20 13:57:16 +0100

    Update header.js

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,14 +1,1 @@
-// header.js
-export function loadHeader() {
-  const header = document.getElementById('header');
-  header.innerHTML = `
-      <nav>
-        <h1>My Portfolio</h1>
-        <ul>
-             <li><a href="#about">About</a></li>
-              <li><a href="#projects">Projects</a></li>
-               <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
-    `;
-}
+
