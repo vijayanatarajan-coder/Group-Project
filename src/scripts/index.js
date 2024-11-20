4bacf35fc715a5b83a69782bd5535956a671b6ad
Vijaya Natarajan <179537302+vijayanatarajan-coder@users.noreply.github.com>
2024-11-20 13:57:46 +0100

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,8 @@
 // index.js
-import { loadHeader } from './header.js';
-import { loadFooter } from './footer.js';
-import { loadMainContent } from './main.js';
 
-// Load modules
-loadHeader();
-loadMainContent();
+import { loadFooter } from './footer.js';
+
+
+
+
 loadFooter();
