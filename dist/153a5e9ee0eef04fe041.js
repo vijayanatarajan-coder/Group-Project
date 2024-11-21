61e5ae1e60275b625d64c45bc9578d12c468692d
vijayanatarajan-coder <179537302+vijayanatarajan-coder@users.noreply.github.com>
2024-11-21 10:34:37 +0100

    Remove dist folder from Git tracking

--- dist/153a5e9ee0eef04fe041.js
+++
@@ -1,9 +0,0 @@
-// index.js
-import { loadHeader } from './header.js';
-import { loadFooter } from './footer.js';
-import { loadMainContent } from './main.js';
-
-// Load modules
-loadHeader();
-loadMainContent();
-loadFooter();
