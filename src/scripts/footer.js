3ce1859923b5a01503b8cb9a403a3a63f050a1b1
Tahir Mehmood <tahir.mehmood@hyperisland.se>
2024-11-21 11:53:11 +0100

    added a header

--- src/scripts/footer.js
+++
@@ -1,7 +0,0 @@
-// footer.js
-export function loadFooter() {
-  const footer = document.getElementById('footer');
-  footer.innerHTML = `
-      <p>&copy; 2024 My Portfolio. All rights reserved.</p>
-    `;
-}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,5 @@
-// index.js
-import { loadHeader } from './header.js';
-import { loadFooter } from './footer.js';
-import { loadMainContent } from './main.js';
 
-// Load modules
+import { loadHeader } from './header.js';
+
 loadHeader();
-loadMainContent();
-loadFooter();
+
